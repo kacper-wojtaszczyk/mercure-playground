521cfde14f3925a4c5335ee167e5c07067997fc7
Kacper Wojtaszczyk <kacper.wojtaszczyk95@gmail.com>
2022-08-05 14:45:05 +0300

    everything

--- src/Application/Port/Cli/CreateProduct.php
+++ src/Application/Port/Cli/CreateProduct.php
@@ -4,24 +4,20 @@ declare(strict_types=1);
 
 namespace App\Application\Port\Cli;
 
-use App\Application\Port\Notification\SummaryNotification;
-use App\Domain\Product\Status;
+use App\Domain\Message\Command\CreateProduct as CreateProductDomainCommand;
+use Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Mercure\HubInterface;
-use Symfony\Component\Mercure\Update;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsCommand(name: 'app:product:create', description: 'Create a new product')]
 final class CreateProduct extends Command
 {
     public function __construct(
-        private readonly HubInterface $hub,
-        private readonly SerializerInterface $serializer,
-        private readonly string $baseUrl,
+        private readonly MessageBusInterface $messageBus
     ) {
         parent::__construct();
     }
@@ -29,21 +25,21 @@ final class CreateProduct extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('send something')
+            ->setDescription('create product')
             ->addArgument('factory', InputArgument::REQUIRED)
+            ->addArgument('name', InputArgument::REQUIRED)
             ->addArgument('status', InputArgument::REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->hub->publish(new Update(
-            $this->baseUrl . '/notifications/product/factory/' . $input->getArgument('factory'),
-            $this->serializer->serialize(
-                new SummaryNotification(Status::from($input->getArgument('status'))),
-                'json'
-            ),
-            true,
-        ));
+        $command = new CreateProductDomainCommand($input->getArgument('factory'), $input->getArgument('name'));
+        try {
+            $this->messageBus->dispatch($command);
+        } catch (Exception $e) {
+            $output->writeln($e->getMessage());
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
